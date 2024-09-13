81cf869b1954f88f270280fa43abe71b9d82f8eb
Admin <dj-ukol@yandex.ru>
2024-09-13 13:37:41 +0500

    Реализация переходов в виде преобразований в другие типы. Добавлен метод reject, который изменяет состояние публикации с PendingReview обратно на DraftPost. Теперь необходимо два вызова метода approve, прежде чем переводить состояние в Post (публикацию).

--- src/State/src/lib.rs
+++ src/State/src/lib.rs
@@ -3,27 +3,70 @@ pub struct Post {
     content: String,
 }
 
+// структура предоставляет черновик
 pub struct DraftPost {
     content: String,
 }
 
-
 impl Post {
-    
-    pub fn new() -> DraftPost {
+    pub fn new() -> DraftPost {     // при создании экземпляра Post, сначала создаем пустой DraftPost (черновик)
         DraftPost {
             content: "".to_string(),
         }
     }
 
+    // запросить содержимое публикации
     pub fn content(&self) -> &str {
         &self.content
     }
 
 }
 
+
 impl DraftPost {
-    pub fn add_text(&mut self, text: &str) {
-        self.content.push_str(text);
+
+    // Добавить текст в черновик
+    pub fn add_text(&mut self, text: &str) {    // обязательно мутабельный self, т.к. мы изменяем content
+        self.content = text.to_string();
+    }
+
+    // запрашиваем проверку
+    pub fn request_review(self) -> PendingReviewPost {  // забирает во владение self и поглащает экземпляр DraftPost
+        PendingReviewPost {     // возвращает экземпляр PendingReviewPost
+            content: self.content,  // с содержимым content
+        }
+    }
+}
+
+// структура ожидание проверки публикации
+pub struct PendingReviewPost {
+    content: String,
+}
+
+impl PendingReviewPost {
+    // одобрить проверку публикации
+    pub fn approve(self) -> DoubleApprove {  // забирает во владение self и поглащает экземпляр PendingReviewPost
+        DoubleApprove {                      // возвращаем экземпляр Post
+                content: self.content,  // с содержимым content
+            }
+    }
+
+    // отклонить
+    pub fn reject(self) -> DraftPost {  // забирает во владение self и поглащает экземпляр PendingReviewPost
+        DraftPost {                      // возвращаем экземпляр DraftPost
+            content: self.content,      // с содержимым content
+        }
+    }
+}
+
+pub struct DoubleApprove {
+    content: String,
+}
+
+impl DoubleApprove {
+    pub fn approve(self) -> Post {  // забирает во владение self и поглащает экземпляр PendingReviewPost
+        Post {                      // возвращаем экземпляр Post
+            content: self.content,  // с содержимым content
+        }
     }
 }
--- src/State/src/main.rs
+++ src/State/src/main.rs
@@ -4,6 +4,18 @@ fn main() {
     let mut post = Post::new();                         // создаем новый черновик записи в блоге
 
     post.add_text("I ate a salad for lunch today");     // добавляем текст в черновик
-    assert_eq!("", post.content());
+
+    let post = post.request_review();
+
+    let mut post = post.reject();
+
+    post.add_text("I ate my salad for my lunch today");
+
+    let post = post.request_review();
+
+    let post = post.approve().approve();    // необходимо двойное подтверждение
+
+    assert_eq!("I ate my salad for my lunch today", post.content());
+
 
 }
